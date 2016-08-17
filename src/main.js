2efa2df66384b9fe81bb12cec661eac9776fd963
fzred <1179750402@qq.com>
2016-08-18 07:53:42 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 import Index from './components/index'
+import App from './components/app'
 
-ReactDOM.render(<Index />, document.getElementById('app'))
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}></Route>
+    <Route path="/index" component={Index}></Route>
+  </Router>
+), document.getElementById('app'))
