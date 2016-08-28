01f26634d88af100df79185551b8681b7ba6f01e
fzred <1179750402@qq.com>
2016-08-28 12:11:40 +0800

    http

---
+++ src/common/http/index.js
@@ -0,0 +1,35 @@
+import Url from './url'
+
+export const options = {
+  root: '/api',
+  method: 'GET',
+  headers: {},
+}
+
+export default function http(url, data, opts) {
+  opts = Object.assign({}, options, opts)
+
+  const uri = new Url(url, options)
+  if (opts.method === 'GET') {
+    uri.addParams(data)
+  }
+  return fetch(uri.getUrl(), opts).then(res => res.json())
+}
+
+export function get(url, data, options) {
+  return http(url, data, Object.assign({ method: 'GET' }, options))
+}
+
+export function post(url, data, options) {
+  return http(url, {}, Object.assign({
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }, options))
+}
+
+http.get = get
+http.post = post
---
+++ src/common/http/url.js
@@ -0,0 +1,30 @@
+export default class Url {
+  constructor(url, options) {
+    if (!url.match(/^(https?:)?\//)) {
+      url = options.root + '/' + url
+    }
+    const el = document.createElement('a')
+    el.href = url
+    this.uri = el
+  }
+
+  addParams(params) {
+    const uri = this.uri
+    let paramsUrl
+    if (typeof params === 'object') {
+      paramsUrl = Object.keys(params).map(key => {
+        return key + '=' + encodeURIComponent(params[key])
+      }).join('&')
+    }
+    if (uri.search.length > 0) {
+      uri.search += '&' + paramsUrl
+    } else {
+      uri.search = paramsUrl
+    }
+    return this
+  }
+
+  getUrl() {
+    return this.uri.href
+  }
+}
