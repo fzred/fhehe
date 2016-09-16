0056d5c9d4a90bb45a044122da98ba57383292e3
fzred <1179750402@qq.com>
2016-09-16 16:45:38 +0800

    update

--- src/components/ImgView.js
+++ src/components/ImgView.js
@@ -2,22 +2,70 @@ import React from 'react'
 import ReactSwipe from 'react-swipe'
 
 export default class extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      boxImg: {
+        show: false,
+        startSlide: 0,
+      }
+    }
+  }
+
+  elSwipe() {
+    let elSwipe
+    if (this.state.boxImg.show) {
+      const boxImg = this.state.boxImg
+      elSwipe = (
+        <div className="img-box" onTouchTap={this.hideBox.bind(this)}>
+          <ReactSwipe
+            swipeOptions={{
+              continuous: false,
+              startSlide: boxImg.startSlide
+            }}
+          >
+            {
+              this.props.imgs.map((item, i) => {
+                return (<img key={i} src={item}/>)
+              })
+            }
+          </ReactSwipe>
+        </div>
+      )
+    }
+    return elSwipe
+  }
+
   render() {
     if (!this.props.imgs || this.props.imgs.length < 1) return <div></div>
     return (
       <div>
-        <ReactSwipe className="carousel" swipeOptions={{ continuous: false }}>
-          <div>PANE 1</div>
-          <div>PANE 2</div>
-          <div>PANE 3</div>
-        </ReactSwipe>
+        {this.elSwipe()}
         {
           this.props.imgs.map((item, i) => {
-            return (<img key={i} src={item}/>)
+            return (
+              <img onTouchTap={this.showBox.bind(this, i)} key={i} src={item}/>
+            )
           })
         }
-        <div></div>
       </div>
     )
+  }
+
+  hideBox() {
+    this.setState({
+      boxImg: {
+        show: false,
+      }
+    })
+  }
+
+  showBox(i) {
+    this.setState({
+      boxImg: {
+        show: true,
+        startSlide: i,
+      }
+    })
   }
 }
