983dddff61a96161bfef1efbc5377703a345361a
fzred <1179750402@qq.com>
2016-09-17 23:51:49 +0800

    update

--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -5,7 +5,7 @@ import { formatDate } from '../common/util'
 export default class extends React.Component {
   render() {
     return (
-      <article>
+      <article className="log-item">
         <time>{formatDate(this.props.item.createTime)}</time>
         <p>{this.props.item.text}</p>
         <ImgView imgs={this.props.item.imgs}></ImgView>
